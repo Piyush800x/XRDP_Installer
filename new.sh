e08f21ebc6fcd239aa382631a702b5aa33a1cdd9
Piyush_Paul <76062260+Piyush800x@users.noreply.github.com>
2022-03-02 16:09:58 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -6,3 +6,7 @@ sudo yum install firewalld -y
 sudo yum install figlet -y
 sudo yum install wget -y
 wget https://raw.githubusercontent.com/Piyush800x/XRDP_Installer/main/new.py
+sleep 5
+figlet XRDP INSTALLER
+sleep 3
+sudo python3 new.py
