7eab6eee8141f681eecbd1fdbca5a379440178cc
Piyush_Paul <76062260+Piyush800x@users.noreply.github.com>
2022-03-02 16:07:58 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -4,3 +4,5 @@ sudo yum install python -y
 sudo yum install nano -y
 sudo yum install firewalld -y
 sudo yum install figlet -y
+sudo yum install wget -y
+wget https://raw.githubusercontent.com/Piyush800x/XRDP_Installer/main/new.py
